73bfc45e7b91695117169d969ccc3730b53b2224
Hor-CN <Hor@itbk.cn>
2022-06-21 17:22:53 +0800

    feature: 添加 NProgress 进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 // 自动配置路由
 import routes from 'virtual:generated-pages'
+// 进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 // 创建 Vue Router 实例
 const router = createRouter({
   // 使用 Hash 模式
   history: createWebHashHistory(),
   routes: routes,
+})
+
+router.beforeEach(async (_to, _from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach((_to) => {
+  NProgress.done()
 })
 
 export default router
