f6f99a20e806eb231474c377d64fa3d10bdbb2fd
Hor-CN <Hor@itbk.cn>
2022-06-21 15:38:35 +0800

    feature: 添加 Less 开启 Less Js 运行时

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,13 @@ export default defineConfig({
       },
     }),
   ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
